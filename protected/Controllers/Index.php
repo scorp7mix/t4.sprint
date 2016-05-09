d843300e58c50d58c2203f5a89370cc83c25c68f
Maxim Fedorov <scorp7mix@gmail.com>
2016-05-09 13:49:01 +0300

    initial commit

---
+++ protected/Controllers/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use T4\Mvc\Controller;
+
+class Index
+    extends Controller
+{
+
+    public function actionDefault()
+    {
+
+    }
+
+    public function actionAbout()
+    {
+
+    }
+
+}
---
+++ protected/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'app_title' => 'My Personal Web Page',
+
+    'extensions' => [
+        'jquery' => [],
+        'bootstrap' => [
+            'theme' => 'cosmo'
+        ]
+    ]
+
+];
